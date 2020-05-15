932ab420f0023949ed76ee966bd0f3e13202cbab
Ben <bigriffin94@outlook.com>
2020-05-14 21:15:31 -0700

    RING buffer MVP complete used DLL and LRU cache logic from Brian Doyle's lecture

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -4,46 +4,51 @@ from doubly_linked_list import DoublyLinkedList
 class RingBuffer:
     def __init__(self, capacity):
         self.capacity = capacity
-        self.order = DoublyLinkedList()
-        self.storage = dict()
+        # use our DLL to store the data
+        self.storage = DoublyLinkedList()
+        self.current = None
 
-    def append(self, key, value):
-        # if item / key already exists
-        if key in self.storage:
-            # overwrite the value
-            # wehre is the value sorted?
-            # we can get the node fro mour storage key
-            node = self.storage[key]
-            # the nodes value is the new tuple with the old key and new value
-            node.value = (key, value)
-            # move value to the tail which is most recently used
-            self.order.move_to_end(node)
-            return  # xit the function
+    def append(self, item):
+        # adds item to the buffer
 
-        # what if our storage is at its capacity??
-        if len(self.order) == self.capacity:
-            # remove the oldest node
-            # we have to references to the last node - in our DLL and in our dict
-            # remove from both
-            index_of_oldest = self.order.head.value[0]
-            # now delete the found index
-            del self.storage[index_of_oldest]
-            # now we may remove it from DLL
-            self.order.remove_from_head()
+        # PLAN #
+        # check to see if our storage length is 0
+        if len(self.storage) == 0:
+            # # if our storage length is 0 then we need to set the incoming item to the head
+            self.storage.add_to_head(item)
+        # # udpate our current variable to be the head as well
+            self.current = self.storage.head
+        # else if the length of our storage is < than capacity - menaing we have room
+        elif len(self.storage) < self.capacity:
+            # # add incoming item to the tail of the list
+            self.storage.add_to_tail(item)
+        # else if the storage is at capacity - check if the current.next prop is none
+        elif len(self.storage) == self.capacity:
+            if self.current.next is not None:
+                # # replace the value of current with the item bc we are at capacity
+                self.current.value = item
+        # # update the curent item to be the next node
+                self.current = self.current.next
+            # otherwise replace current.value with incoming item
+            else:
+                self.current.value = item
+            # # update the curent to be the head of our storage
+                self.current = self.storage.head
 
-        # finally add newest node to end/tail and add it back to storage
-        self.order.add_to_tail((key, value))
-        self.storage[key] = self.order.tail
+    def get(self):
+        # returns all of the elements in the buffer in a list in their given order
+        # does not return any NONE values in the list even if present
 
-    def get(self, key):
-        # if key does not exist in our cache return none
-        if key not in self.storage:
-            return None
-        else:
-            # if key in cache
-            # # then move it to most recently used
-            # # create a place holder for our node in our storage
-            node = self.storage[key]
-            self.order.move_to_end(node)
-            # return the value of node in its tuple whise is equal to index[1]
-            return node.value[1]
+        ## PLAN ##
+        # create a list to hold all elements in ring buffer
+        item_list = list()
+        # create a var to track the head of our buffer
+        head_node = self.storage.head
+        # while we have a head / ring buffer
+        while head_node is not None:
+            # - append the value to our list
+            item_list.append(head_node.value)
+        # # udpate our var tracking the head to be the next node in the list so that each loop it moves to the next item
+            head_node = head_node.next
+        # once loop is exited we will return the list
+        return item_list
